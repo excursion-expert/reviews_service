de8388ef82b4581b73da55e798e99fea71ab98fe
Marissa Romero <marissakarenromero@gmail.com>
2020-11-12 21:00:24 -0800

    Added API calls to fetch reviews and specific user.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const db = require('../database');
+const db = require('../database/index.js');
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
@@ -12,5 +12,34 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.get('/reviews', function (req, res) {
-	// res.send(‘Hello World’)
+
+	const sql = `SELECT * FROM reviews`;
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.send(data);
+    }
+  })
+
+})
+
+app.get('/reviews/:userId', function (req, res) {
+
+	var userId = req.params.userId;
+
+
+	const sql = `SELECT * FROM users WHERE user_id = ` + db.escape(userId);
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.send(data);
+    }
+  })
+
 })
